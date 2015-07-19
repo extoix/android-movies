3a435eadf1a694dc786792202fe395d6618ce048
John Kuk <extoix@users.noreply.github.com>
2015-07-18 22:36:23 -0500

    Added Picasso dependencies to gradle. Added internet permission to the manifest. Modified fragment to add an ImageView. Called Picasso and loaded an image into the ImageView.

--- Movies/app/src/main/java/com/extoix/android/movies/MainActivityFragment.java
+++ Movies/app/src/main/java/com/extoix/android/movies/MainActivityFragment.java
@@ -1,10 +1,13 @@
 package com.extoix.android.movies;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 
 /**
@@ -16,8 +19,12 @@ public class MainActivityFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        ImageView imageView = (ImageView)rootView.findViewById(R.id.movie_poster_imageview);
+        Picasso.with(getActivity()).load("http://i.imgur.com/DvpvklR.png").into(imageView);
+
+        return rootView;
     }
 }
