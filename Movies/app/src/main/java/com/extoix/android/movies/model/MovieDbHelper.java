2535962f6ffa3dc7652a16e7e22a94ca89a135cf
John Kuk <extoix@users.noreply.github.com>
2015-09-07 12:39:25 -0500

    Did some cleanup of the Trailer create SQL in MovieDbHelper.

--- Movies/app/src/main/java/com/extoix/android/movies/model/MovieDbHelper.java
+++ Movies/app/src/main/java/com/extoix/android/movies/model/MovieDbHelper.java
@@ -38,16 +38,11 @@ public class MovieDbHelper extends SQLiteOpenHelper {
                 + TrailerEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
                 + TrailerEntry.MOVIE_KEY + " INTEGER NOT NULL, "
                 + TrailerEntry.KEY + " TEXT NOT NULL, "
-                + TrailerEntry.NAME + " TEXT NOT NULL, " +
-
-                // Set up the location column as a foreign key to location table.
-                " FOREIGN KEY (" + TrailerEntry.MOVIE_KEY+ ") REFERENCES " +
-                MovieEntry.TABLE_NAME + " (" + MovieEntry._ID + "), " +
-
-                // To assure the application have just one weather entry per day
-                // per location, it's created a UNIQUE constraint with REPLACE strategy
-                " UNIQUE (" + TrailerEntry.KEY + ", " +
-                TrailerEntry.MOVIE_KEY + ") ON CONFLICT REPLACE);";
+                + TrailerEntry.NAME + " TEXT NOT NULL, "
+                + " FOREIGN KEY (" + TrailerEntry.MOVIE_KEY+ ")"
+                + " REFERENCES " + MovieEntry.TABLE_NAME + " (" + MovieEntry._ID + "), "
+                + " UNIQUE (" + TrailerEntry.KEY + ", " + TrailerEntry.MOVIE_KEY + ")"
+                + " ON CONFLICT REPLACE);";
 
         db.execSQL(SQL_CREATE_MOVIE_TABLE);
         db.execSQL(SQL_CREATE_TRAILER_TABLE);
