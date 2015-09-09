bcaef8e12461e6ce13d8d5104205c0cf3c658abc
John Kuk <extoix@users.noreply.github.com>
2015-09-08 22:59:43 -0500

    Initial addition of a Master-Detail layout for Nexus 7. Renamed the main fragment to movie since that makes more logical sense. Removed the values-w820dp directory and its contents.

--- Movies/app/src/main/java/com/extoix/android/movies/ui/MovieFragment.java
+++ Movies/app/src/main/java/com/extoix/android/movies/ui/MovieFragment.java
@@ -78,7 +78,7 @@ public class MovieFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_movie, container, false);
 
         GridView moviePosterGridView = (GridView) rootView.findViewById(R.id.movie_poster_gridview);
 
