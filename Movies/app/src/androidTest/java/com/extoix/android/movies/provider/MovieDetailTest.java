49e5c7775c39a0e45be752c9243a7b816f1490ba
extoix <extoix@users.noreply.github.com>
2015-09-25 23:02:41 -0500

    Added a test to create data in the MovieDetail and test the data was added successfully.

--- Movies/app/src/androidTest/java/com/extoix/android/movies/provider/MovieDetailTest.java
+++ Movies/app/src/androidTest/java/com/extoix/android/movies/provider/MovieDetailTest.java
@@ -1,10 +1,13 @@
 package com.extoix.android.movies.provider;
 
+import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.test.AndroidTestCase;
 
 import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 import com.extoix.android.movies.provider.MovieDetailContract.MovieDetailEntry;
 
@@ -38,18 +41,18 @@ public class MovieDetailTest extends AndroidTestCase {
         assertEquals(true, db.isOpen());
 
 
-        Cursor c = db.rawQuery("SELECT name FROM sqlite_master WHERE type='table'", null);
-        assertTrue("Error:  The database has not been created correctly", c.moveToFirst());
+        Cursor cursor = db.rawQuery("SELECT name FROM sqlite_master WHERE type='table'", null);
+        assertTrue("Error:  The database has not been created correctly", cursor.moveToFirst());
 
 
         do {
-            tableNameHashSet.remove(c.getString(0));
-        } while( c.moveToNext() );
+            tableNameHashSet.remove(cursor.getString(0));
+        } while( cursor.moveToNext() );
         assertTrue("Error:  Your database was created without the movie details table", tableNameHashSet.isEmpty());
 
 
-        c = db.rawQuery("PRAGMA table_info(" + MovieDetailEntry.TABLE_MOVIE_DETAIL + ")", null);
-        assertTrue("Error:  Unable to query the database for table information.", c.moveToFirst());
+        cursor = db.rawQuery("PRAGMA table_info(" + MovieDetailEntry.TABLE_MOVIE_DETAIL + ")", null);
+        assertTrue("Error:  Unable to query the database for table information.", cursor.moveToFirst());
 
 
         final HashSet<String> movieDetailColumnHashSet = new HashSet<String>();
@@ -61,11 +64,60 @@ public class MovieDetailTest extends AndroidTestCase {
         movieDetailColumnHashSet.add(MovieDetailEntry.COLUMN_POSTER_PATH);
         movieDetailColumnHashSet.add(MovieDetailEntry.COLUMN_POSTER_PATH_URL);
 
-        int columnNameIndex = c.getColumnIndex("name");
+        int columnNameIndex = cursor.getColumnIndex("name");
         do {
-            String columnName = c.getString(columnNameIndex);
+            String columnName = cursor.getString(columnNameIndex);
             movieDetailColumnHashSet.remove(columnName);
-        } while(c.moveToNext());
+        } while(cursor.moveToNext());
         assertTrue("Error: The database doesn't contain all of the required location entry columns", movieDetailColumnHashSet.isEmpty());
+
+        cursor.close();
+    }
+
+    public void testMovieDetailTable() {
+
+        MovieDetailDBHelper movieDetailDBHelper = new MovieDetailDBHelper(mContext);
+        SQLiteDatabase db = movieDetailDBHelper.getWritableDatabase();
+
+        ContentValues testValues = createMovieDetailValues();
+
+        long movieDetailRowId = db.insert(MovieDetailEntry.TABLE_MOVIE_DETAIL, null, testValues);
+        assertTrue(movieDetailRowId != -1);
+
+
+        Cursor cursor = db.query(MovieDetailEntry.TABLE_MOVIE_DETAIL, null, null, null, null, null, null);
+        assertTrue("Error: No Records returned from location query", cursor.moveToFirst());
+
+
+        validateCurrentRecord("Error: Movie Detail Query Validation Failed", cursor, testValues);
+        assertFalse("Error: More than one record returned from location query", cursor.moveToNext());
+
+        cursor.close();
+    }
+
+    static ContentValues createMovieDetailValues() {
+        ContentValues testValues = new ContentValues();
+
+        testValues.put(MovieDetailEntry.COLUMN_ID, "76341");
+        testValues.put(MovieDetailEntry.COLUMN_TITLE, "Mad Max: Fury Road");
+        testValues.put(MovieDetailEntry.COLUMN_RELEASE_DATE, "2015-05-15");
+        testValues.put(MovieDetailEntry.COLUMN_VOTE_AVERAGE, "7.6");
+        testValues.put(MovieDetailEntry.COLUMN_OVERVIEW, "An apocalyptic story set in the furthest reaches of our planet, in a stark desert landscape where humanity is broken, and most everyone is crazed fighting for the necessities of life. Within this world exist two rebels on the run who just might be able to restore order. There's Max, a man of action and a man of few words, who seeks peace of mind following the loss of his wife and child in the aftermath of the chaos. And Furiosa, a woman of action and a woman who believes her path to survival may be achieved if she can make it across the desert back to her childhood homeland.");
+        testValues.put(MovieDetailEntry.COLUMN_POSTER_PATH, "/kqjL17yufvn9OVLyXYpvtyrFfak.jpg");
+        testValues.put(MovieDetailEntry.COLUMN_POSTER_PATH_URL, "http://image.tmdb.org/t/p/w184/kqjL17yufvn9OVLyXYpvtyrFfak.jpg");
+
+        return testValues;
+    }
+
+    static void validateCurrentRecord(String error, Cursor valueCursor, ContentValues expectedValues) {
+        Set<Map.Entry<String, Object>> valueSet = expectedValues.valueSet();
+        for (Map.Entry<String, Object> entry : valueSet) {
+            String columnName = entry.getKey();
+            int index = valueCursor.getColumnIndex(columnName);
+            assertFalse("Column '" + columnName + "' not found. " + error, index == -1);
+
+            String expectedValue = entry.getValue().toString();
+            assertEquals("Value '" + entry.getValue().toString() + "' did not match the expected value '" + expectedValue + "'. " + error, expectedValue, valueCursor.getString(index));
+        }
     }
 }
